87a645869a56dab7cb55480229b183b0c3babd24
Hocine <149161032+ChHocine@users.noreply.github.com>
2025-01-09 12:11:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.getElementById('question-form').addEventListener('submit', function(e) {
-    e.preventDefault();
 
-    const question = document.getElementById('question').value;
-
-    if (question.trim() !== "") {
-        alert("Your question has been received. We will get back to you with an answer sourced from credible books.");
-        document.getElementById('question').value = "";  
-    } else {
-        alert("Please type your question.");
-    }
-});
